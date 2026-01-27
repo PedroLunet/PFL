1b76c97964a8eb1d4535868b02c21a1309ee08d2
Pedro Lunet <pedrolunet@gmail.com>
2026-01-27 14:30:52 +0000

    h-99 ex13

---
+++ haskell/H-99/ex13.hs
@@ -0,0 +1,37 @@
+data ListItem a = Single a | Multiple Int a
+  deriving (Show)
+
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack  [a] = [[a]]
+pack (x : y : xs)
+  | x == y = (takeWhile (== x) (x : y : xs)) : pack (dropWhile (== x) (x : y : xs))
+  | otherwise = [x] : pack (y : xs)
+
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode [] = []
+encode xs = [(length temp, head temp) | temp <- pack xs]
+
+encodeModified :: Eq a => [a] -> [ListItem a]
+encodeModified [] = []
+encodeModified xs = map aux (encode xs)
+  where
+    aux (i, c) 
+      | i == 1 = (Single c)
+      | otherwise = (Multiple i c)
+
+decodeModified :: [ListItem a] -> [a]
+decodeModified = concatMap expand
+  where
+    expand (Single x) = [x]
+    expand (Multiple 0 _) = []
+    expand (Multiple n x) = x : expand (Multiple (n-1) x)
+
+encodeDirect :: (Eq a) => [a] -> [ListItem a]
+encodeDirect [] = []
+encodeDirect (x:xs)
+  | count == 1 = (Single x) : encodeDirect rest
+  | otherwise = (Multiple count x) : encodeDirect rest
+  where
+    (matched, rest) = span (== x) (x:xs)
+    count = length matched
