07cc67a5e94682feeaf5aa577f09d260e8923e33
Pedro Lunet <pedrolunet@gmail.com>
2026-01-27 21:30:02 +0000

    ex1 mt12425

---
+++ haskell/mt12425/ex1.hs
@@ -0,0 +1,16 @@
+type Match = ((String,String), (Int,Int))
+type MatchDay = [Match]
+type League = [MatchDay]
+
+myLeague :: League
+myLeague = [
+  [(("Porto","Sporting"),(2,2)),(("Benfica","Vitoria SC"),(4,0))],
+  [(("Porto","Benfica"),(5,0)),(("Vitoria SC","Sporting"),(3,2))],
+  [(("Vitoria SC","Porto"),(1,2)),(("Sporting","Benfica"),(2,1))]
+  ]
+
+winner :: Match -> String 
+winner ((ta, tb),(ga, gb))
+  | ga > gb = ta
+  | gb > gb = tb
+  | otherwise = "draw"
