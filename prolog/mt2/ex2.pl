89f03bcd94bf4fc5fc3bdafaa5baad385ea77831
Pedro Lunet <pedrolunet@gmail.com>
2026-01-28 11:16:42 +0000

    ex2 mt2 2526

---
+++ prolog/mt2/ex2.pl
@@ -0,0 +1,15 @@
+% bird(Name, Species, Gender, Colors)
+bird(robinho,  robin,   male,   [red, brown, white]).
+bird(robina,     robin,   female, [brown, red, white]).
+bird(ferrugem,   robin,   male,   [brown, gray]).
+bird(arcoiris,   parrot,  male,   [red, blue, green, yellow]).
+bird(verdeja,    parrot,  female, [green, yellow]).
+bird(minerva,    owl,     female, [brown, white]).
+bird(noctis,     owl,     male,   [gray, white]).
+bird(sabia,      owl,     female, [beige, brown]).
+
+has_more_color_of(Name, GreaterColor, LesserColor):-
+    bird(Name, _Species, _Gender, Colors),
+    append(_, [GreaterColor|T], Colors),
+    append(_, [LesserColor|_], T).
+
